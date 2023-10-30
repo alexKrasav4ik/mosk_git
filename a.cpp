5166161f29767104a4108bbe6468d9a5b6b4a6df
alexKrasav4ik <panarin.04@inbox.ru>
2023-10-30 19:31:40 +0300

    smth

---
+++ a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int a[2];
+	a[0] = 1;
+	a[1] = 2;
+	cout << a[14312];
+	return 0;
+}
--- a.py
+++ a.py
@@ -1,1 +1,22 @@
-print("Hello world")
+a = [15, 10, 20, '15', 20.27]
+print(a[10])
+# условный оператор
+#a = int(input())
+#if a % 2 == 0:
+#	print('Vasya')
+#else:
+#	print('Petya')
+
+'''
+цикл for
+for i in range(10):
+	if i % 2 == 1:
+		print(i * 5)
+	else:
+		print(0)
+'''
+'''a = int(input())
+while a > 0:
+	b = a % 2
+	print(b)
+	a = a // 2'''
